064351b42b42d2ec21977a1d45ecba2233de9f78
Futaroukun <93515103+Futaroukun@users.noreply.github.com>
2022-09-19 16:11:12 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • Triii [62895604670507]
-│ • Gopay  [62895604670507]
-│ • Dana  [62895604670507]
-│ • Qris  [Chat OWNER]
+│ • Axis [6283854551575]
+│ • Gopay  [6285722157719]
+│ • Dana  [6285722157719]
+│ • Qris  [Belum Punya]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6283854551575
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
